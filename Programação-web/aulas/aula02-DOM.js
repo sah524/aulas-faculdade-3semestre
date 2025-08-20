808cf3498ede7e1dc3e962c2b746985b286e1ea3
Sarahh <sarahtangela@gmail.com>
2025-08-20 10:59:46 -0300

    Conteúdo da aula 02

---
+++ Programação-web/aulas/aula02-DOM.js
@@ -0,0 +1,55 @@
+//buscar o elemento na página HTML
+
+let meuElemento = document.getElementById("paragrafo");
+console.log(meuElemento);
+console.log(meuElemento.textContent);
+
+let paragrafo1 = document.getElementsByClassName("paragrafo");
+console.log(paragrafo1);
+
+//laço de repetição
+
+for (let i=0 ; i<paragrafo1.length; i++){
+    console.log(paragrafo1[i].textContent);
+}
+
+console.log(paragrafo1[2].textContent);
+
+let paragrafo2 = document.getElementsByTagName("p");
+console.log(paragrafo2);
+
+//criar um elemento via JavaScript
+let destino = document.getElementById("elemento")
+let p = document.createElement("p");
+p.textContent = "Paragrafo criado via JavaScript";
+destino.append(p);
+
+//criar uma lista 
+let ul = document.createElement("ul");
+let itens = ["Item 1", "Item 2"];
+for(let i=0; i<itens.length; i++){
+    let li = document.createElement("li");
+    li.textContent = itens[i];
+    ul.append(li); //adiciona na lista 
+}
+destino.append(ul);
+
+//função de somar()
+
+function somar(){
+    let n1 = parseFloat(document.getElementById("num1").value);
+    console.log(typeof n1)
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let soma = n1 + n2;
+    console.log(soma);
+
+    let saida = `<strong>Resultado: <strong> ${soma}`;
+    Document.getElementById("resultado").innerHTML = saida ;
+}
+
+let botao = document.getElemnetById("botao1");
+botao.onclick = function(){
+    alert("Clicou!");
+    botao.textContent = "Você clicou!";
+    botao.style.backgroundColor = "red";
+}
