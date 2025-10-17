0043095824ba69e96dd25b8f525436ad736e8fb1
Sarahh <sarahtangela@gmail.com>
2025-10-17 10:54:27 -0300

    Update Exercicios.sql

--- Laboratorio-de-Banco-de-Dados/aulas/Exercicios.sql
+++ Laboratorio-de-Banco-de-Dados/aulas/Exercicios.sql
@@ -63,8 +63,49 @@ create view qtdlivros as
 select l.titulo, a.autor, count(*)
 from autor a
 inner join livro l on l.autorID = a.ID
-where livro 
+where livro ;
 
--- 06) Apresente quanttos livros foram alugados por pessoa apenas nos anos de 2024 e 2026, ordene por mais alugou livros
+
+
+-- 06) Apresente quantos livros foram alugados por pessoa apenas nos anos de 2024 e 2026, ordene por mais alugou livros
 
 -- 07)Mostre quanto cada cliente tem que pagar e quanto do saldo sobrará caso realize o pagamento
+-- select p.ID , p.nome , l*( case 
+-- 	when timestampdiff(day, data_emprestimo , data_devolucao) < 7 
+ --    then 0
+-- 	else timestampdiff(day, data_emprestimo , data_devolucao) - 7 
+-- 		end) 
+-- 	end) as atraso
+
+-- from Emprestimo e ;
+
+
+
+
+create user atendente@localhost identified by "12345678";
+
+grant select on biblioteca.autor to atendente@localhost;
+
+create view vw_dados as 
+select 
+ nome as nome_completo
+ from autor;
+ 
+ grant select on biblioteca.vw_dados to atendente@localhost;
+ 
+ 
+
+select * from autor;
+
+
+
+create user gerente@localhost identified by "12345678";
+
+grant all on biblioteca.* to gerente@localhost;
+
+create view qtdlivros as
+select l.titulo, a.autor, count(*)
+from autor a
+inner join livro l on l.autorID = a.ID
+where livro ;
+
